6c18b61dc2cb0c447d6c5b8d6ebe10624c53bace
eyee19 <eyee305@gmail.com>
2018-11-29 10:50:57 -0800

    Added onClick for saved recipes to take to browser

--- app/src/main/java/com/example/eyee3/fridgemate/SavedAdapter.java
+++ app/src/main/java/com/example/eyee3/fridgemate/SavedAdapter.java
@@ -1,12 +1,17 @@
 package com.example.eyee3.fridgemate;
 
 import android.content.Context;
+import android.content.Intent;
 import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.net.Uri;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class SavedAdapter extends RecyclerView.Adapter<SavedAdapter.SavedViewHolder> {
     private Context sContext;
@@ -39,10 +44,21 @@ public class SavedAdapter extends RecyclerView.Adapter<SavedAdapter.SavedViewHol
         }
 
         String nameS = sCursor.getString(sCursor.getColumnIndex(SavedContract.SavedEntry.COLUMN_NAMES));
+        final String linkS = sCursor.getString(sCursor.getColumnIndex(SavedContract.SavedEntry.COLUMN_LINK));
         long id = sCursor.getLong(sCursor.getColumnIndex(SavedContract.SavedEntry._ID));
 
         savedViewHolder.savedName.setText(nameS);
         savedViewHolder.itemView.setTag(id);
+
+        savedViewHolder.savedName.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String url = linkS;
+                Intent i = new Intent(Intent.ACTION_VIEW);
+                i.setData(Uri.parse(url));
+                sContext.startActivity(i);
+            }
+        });
     }
 
     @Override
